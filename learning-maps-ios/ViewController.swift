bbb5e326a7ac463a64923445f5babb20e911907f
Emerson Novais <emersonnovais93@gmail.com>
2022-04-13 12:06:06 -0300

    Learning use maps and geolocation
    First commit

--- learning-maps-ios/ViewController.swift
+++ learning-maps-ios/ViewController.swift
@@ -6,12 +6,26 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MKMapViewDelegate {
 
+    @IBOutlet weak var mapReference: MKMapView!
+    
+    let latitudeValue: CLLocationDegrees = -22.69896843334008
+    let longitudeValue: CLLocationDegrees = -46.987800181880765
+    let deltaLatitude: CLLocationDegrees = 0.001
+    let deltaLongitude: CLLocationDegrees = 0.001
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let areaVisualization: MKCoordinateSpan = MKCoordinateSpan.init(latitudeDelta: deltaLatitude, longitudeDelta: deltaLongitude)
+        let location: CLLocationCoordinate2D = CLLocationCoordinate2DMake(latitudeValue, longitudeValue)
+        
+        let localRegion: MKCoordinateRegion = MKCoordinateRegion.init(center: location, span: areaVisualization)
+        
+        mapReference.setRegion(localRegion, animated: true)
     }
 
 
